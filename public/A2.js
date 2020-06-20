d353531941af5052fa0ed32444ce07f7a58a5ced
David Du <david2001.du@gmail.com>
2020-06-19 21:54:47 -0700

    attempt to at adding new people

--- public/A2.js
+++ public/A2.js
@@ -2,10 +2,10 @@
 //const other = require('./other');
 
 document.getElementById("adduser").addEventListener('click',(evt)=>{
-  var uname = document.getElementById(Name).body.Name;
-  var usize = document.getElementById(Size).body.Size;
-  var uheight = document.getElementById(Height).body.Height;
-  var utype = document.getElementById(Type).body.Type;
+  var uname = document.getElementById(Name).values;
+  var usize = document.getElementById(Size).values;
+  var uheight = document.getElementById(Height).values;
+  var utype = document.getElementById(Type).values;
   common.pool.query(`insert into people values (uname,usize,uheight,utype)`);
   console.log("inserted");
 });
