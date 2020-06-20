c86595bc3e47c146104553594df377fd0da57321
David Du <david2001.du@gmail.com>
2020-06-20 10:16:26 -0700

    writing query command

--- public/A2.js
+++ public/A2.js
@@ -2,10 +2,16 @@
 //const other = require('./other');
 
 document.getElementById("adduser").addEventListener('click',(evt)=>{
-  var uname = document.getElementById(Name).values;
-  var usize = document.getElementById(Size).values;
-  var uheight = document.getElementById(Height).values;
-  var utype = document.getElementById(Type).values;
-  common.pool.query(`insert into people values (uname,usize,uheight,utype)`);
+  var uname = document.getElementById(Name).value;
+  var usize = document.getElementById(Size).value;
+  var uheight = document.getElementById(Height).value;
+  var utype = document.getElementById(Type).value;
+  var getUsersQuery = `insert into people values (uname,usize,uheight,utype);`
+  common.pool.query(getUsersQuery,(error,result)=>{
+    if(error)
+      res.end(error)
+    results = {'rows':result.rows}
+    res.render('pages/db', results);
+  })
   console.log("inserted");
 });
