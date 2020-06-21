ba03d6365ab725f4e9049b73f654faee6656fee9
David Du <david2001.du@gmail.com>
2020-06-21 10:01:40 -0700

    done for the night

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/Database', (req,res) => {
     if(error)
       res.end(error)
     results = {'rows':result.rows}
-    console.log(results.name);
+    console.log(results[0]);
     //res.render('pages/db', results);
   })
 
