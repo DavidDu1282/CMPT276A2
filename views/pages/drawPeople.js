a916a2f269a53f728b432c84e82f9380bdbc9b68
David Du <david2001.du@gmail.com>
2020-06-21 20:19:20 -0700

    attempting to delete

--- views/pages/drawPeople.js
+++ views/pages/drawPeople.js
@@ -1,3 +1,12 @@
 document.getElementById("").addEventListener('click',(evt)=>{
 
 });
+<br >
+<select name="attribute" id="attribute">
+  <option value="Name">Name</option>
+  <option value="Size">Size</option>
+  <option value="Height">Height</option>
+  <option value="Type">Type</option>
+</select>
+<br >
+<input type ="New tex" name="sub" value = "Add to database"><br>
